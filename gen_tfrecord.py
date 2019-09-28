4dd1ee50f020ba50d9d0b96a9088a26336be970e
b02902063 <fd20090aq@gmail.com>
2019-09-28 23:26:25 +0800

    Update gen_tfrecord.py

--- gen_tfrecord.py
+++ gen_tfrecord.py
@@ -1,6 +1,6 @@
 from utils.data_reader import DataReader
 import sys
 
-d = DataReader("./data/test_images", "./data/sample_submission.csv", int(sys.argv[1]), int(sys.argv[2]))
-d.write_tfrecord("./TFRecord/test/{0}.tfrecord".format(sys.argv[1]))
+d = DataReader("./data/train_images", "./data/train.csv", int(sys.argv[1]), int(sys.argv[2]))
+d.write_tfrecord("./TFRecord/train/{0}.tfrecord".format(sys.argv[1]))
 
